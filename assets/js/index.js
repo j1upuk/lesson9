953f0ab1dc287d546208efc83eacbfaf70e97b5d
j1upuk <j1upuk1@gmail.com>
2020-08-08 17:56:02 +0300

    функцыия

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,14 @@
 "use strict";
 
-let colorRed = "Kirill";
+let f;
+f = prompt ('fbgdui');
 
-console.log(colorRed);
+function bul (num){
+  if (num%2===0){
+  return true;
+  }
+  return false;
+   }
+ const num = bul(f);
+ console.log(num);
 
-//console.log (string)
-
-let string = `my name is ${colorRed}`;
-
-let chek = false;
-
-let frN = prompt("число");
-let a = 0;
-while (+frN > a) {
-  console.log("#");
-  a++;
-}
-
-let frN2 = prompt("число");
-let b = 0;
-do {
-  console.log("#");
-  b++;
-} while (+frN2 > b);
-
-let frN3 = prompt("число");
-
-for (let i = 0; i < +frN3; i++) {
-  console.log("#");
-}
